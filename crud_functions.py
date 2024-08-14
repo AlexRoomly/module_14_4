082938868c574eb21dbf9ac3ba5ada20a8d38263
AlexRoomly <roomly2@gmail.com>
2024-08-14 18:15:40 +0300

    Add files via upload

---
+++ crud_functions.py
@@ -0,0 +1,29 @@
+import sqlite3
+import asyncio
+connection = sqlite3.connect('product_data.db')
+cursor= connection.cursor()
+
+def initiate_db():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INT,
+    title TEXT,
+    description TEXT,
+    price INT
+    )
+    '''),
+    connection.commit()
+
+initiate_db()
+
+# вариант 1
+def get_all_products():
+    connection = sqlite3.connect('product_data.db')
+    cursor = connection.cursor()
+    products=cursor.execute('SELECT * FROM Products').fetchall()
+    connection.commit()
+    return products
+
+
+connection.commit()
+connection.close()
